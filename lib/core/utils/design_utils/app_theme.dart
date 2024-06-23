97b10a7927866a990c274350b59f5579e46c765f
sheha <shehabehab1029@gmail.com>
2024-06-23 19:29:16 +0300

    add new file

--- lib/core/utils/design_utils/app_theme.dart
+++ lib/core/utils/design_utils/app_theme.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 part 'app_colors.dart';
 part 'app_sizes.dart';
 part 'app_text_styles.dart';
-
+part 'app_text_scale.dart';
 abstract final class AppTheme {
   static ThemeData defaultTheme = ThemeData();
 }
