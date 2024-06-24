e8dae1a6a2cfad10f760d4e369aa4caa5ee29020
sheha <shehabehab1029@gmail.com>
2024-06-24 03:10:38 +0300

    edit in repository

--- lib/features/news/domain/repositories/article_repository.dart
+++ lib/features/news/domain/repositories/article_repository.dart
@@ -1,10 +1,6 @@
 import 'package:news_app_task/core/utils/api_utils/data_response.dart';
 import 'package:news_app_task/features/news/domain/entities/article.dart';
 
-abstract interface class ProductsRepositoryInterface {
+abstract interface class ArticlesRepositoryInterface {
   Future<Response<List<Article>>> getBusinessArticles();
-
-  Future<Response<List<Article>>> getSportsArticles();
-
-  Future<Response<List<Article>>> getScienceArticles();
 }
