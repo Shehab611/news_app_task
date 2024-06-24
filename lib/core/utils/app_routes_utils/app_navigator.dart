65bc097a6e38059788281f201be888d210f93ea1
sheha <shehabehab1029@gmail.com>
2024-06-24 04:55:12 +0300

    add screens files and add them to app router files

--- lib/core/utils/app_routes_utils/app_navigator.dart
+++ lib/core/utils/app_routes_utils/app_navigator.dart
@@ -1,10 +1,11 @@
 part of 'app_router.dart';
 
 abstract final class AppNavigator {
-  static navigateToDetailsScreen(BuildContext context) {
+  static navigateToDetailsScreen(BuildContext context,Article article) {
     return Navigator.pushNamed(
       context,
       AppPathName.kNewsDetailsScreen,
+      arguments: article
     );
   }
 }
--- lib/core/utils/app_routes_utils/app_router.dart
+++ lib/core/utils/app_routes_utils/app_router.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_task/features/news/domain/entities/article.dart';
+import 'package:news_app_task/features/news/presentation/views/news_details_screen.dart';
+import 'package:news_app_task/features/news/presentation/views/news_screen.dart';
 
 part 'app_navigator.dart';
 part 'app_paths.dart';
 
 abstract final class AppRouter {
   static final Map<String, WidgetBuilder> routes = {
-
+  AppPathName.kNewsScreen:(BuildContext context)=>const NewsScreen(),
+  AppPathName.kNewsDetailsScreen:(BuildContext context)=>const NewsDetailsScreen(),
   };
 }
